7be6a844f65ddd1f44c0c5b5c771df2fac86100f
codenotkey <chensenzero@foxmail.com>
2021-09-03 10:58:25 +0800

    项目打包

---
+++ precache-manifest.5a125ca28357e990ec6d88df00fa2e05.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d23d295ba6f154ae93f",
+    "url": "/BillProduct2/css/app.6035f053.css"
+  },
+  {
+    "revision": "3215392249a4aa57e932",
+    "url": "/BillProduct2/css/chunk-vendors.282369e9.css"
+  },
+  {
+    "revision": "0485a9b9a2b1f3342e043333977a4a3b",
+    "url": "/BillProduct2/fonts/fzyh.0485a9b9.ttf"
+  },
+  {
+    "revision": "70f97912e44f795b78b48a1a586161b0",
+    "url": "/BillProduct2/fonts/方正悠黑_509R.70f97912.ttf"
+  },
+  {
+    "revision": "023e077565d9b1e4982967e6025364a4",
+    "url": "/BillProduct2/fonts/方正悠黑_510M.023e0775.ttf"
+  },
+  {
+    "revision": "3e88f1939d8792e09ff72e4873ef692d",
+    "url": "/BillProduct2/img/微信支付.3e88f193.png"
+  },
+  {
+    "revision": "e78dbe8a573949f7501e6b03f999193e",
+    "url": "/BillProduct2/index.html"
+  },
+  {
+    "revision": "6d23d295ba6f154ae93f",
+    "url": "/BillProduct2/js/app.dd389e1a.js"
+  },
+  {
+    "revision": "3215392249a4aa57e932",
+    "url": "/BillProduct2/js/chunk-vendors.3831f8bc.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/BillProduct2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/BillProduct2/robots.txt"
+  }
+]);
